a05191e5e0ad51e4468d2394cd6c511c69e48438
diegopresent <luisdiego362@gmail.com>
2026-02-27 15:52:50 -0400

    fix: allowedOrigins

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ const app = express();
 // --- CONFIGURACIÓN DE CORS (Segura y compatible) ---
 const allowedOrigins = [
     'http://localhost:5173',
-    'http://localhost:3000',
-    'https://inventario-frontend-react-vite.vercel.app'
+    'http://localhost:3000'
 ];
 
 if (process.env.FRONTEND_URL) {
